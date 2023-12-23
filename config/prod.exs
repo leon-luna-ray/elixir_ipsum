e0749c5b0ec9d280b1f190965e92173110928607
Ray Luna <leon.luna.ray@gmail.com>
2023-12-22 18:35:12 -0800

    update prod settings

--- config/prod.exs
+++ config/prod.exs
@@ -4,6 +4,6 @@ config :elixir_ipsum, ElixirIpsumWeb.Endpoint,
   url: [host: "elixir-ipsum.fly.dev", port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-config :logger, level: :info
+config :argon2_elixir, t_cost: 2, m_cost: 15 # ~60ms
 
 import_config "prod.secret.exs"

4d4f3cb3b9bbb5f3561331d4df6bcc09d3863b74
Ray Luna <leon.luna.ray@gmail.com>
2023-12-23 09:27:54 -0800

    update port settings

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,10 @@
 import Config
 
 config :elixir_ipsum, ElixirIpsumWeb.Endpoint,
+  http: [port: 8080],
   url: [host: "elixir-ipsum.fly.dev", port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-config :argon2_elixir, t_cost: 4, m_cost: 16 # ~60ms
+config :bcrypt_elixir, t_cost: 4, m_cost: 16 # ~60ms
 
 import_config "prod.secret.exs"
--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule ElixirIpsum.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
